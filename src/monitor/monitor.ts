779b8f1cf29068142ac5fb30a2773df59ac2eacc
0xkydo <kydo0x@gmail.com>
2023-01-16 14:25:10 -0800

    add monitoring module.
    Monitoring module will send SMS notif when MarabuNode disconnects.

---
+++ src/monitor/monitor.ts
@@ -0,0 +1,49 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Set environment variables for your credentials
+// Read more at http://twil.io/secure
+import Net from 'net';
+
+import { canonicalize } from "json-canonicalize";
+import hello from "../FIXED_MESSAGES/hello_test.json";
+
+const accountSid = "AC1b6e54d2f688b377568f344751ae23df";
+const authToken = "359b5203d583c241b0fac691f6952bc5";
+const cell = require("twilio")(accountSid, authToken);
+
+
+// The port number and hostname of the server.
+const port = 18018;
+const host = '54.67.110.108';
+
+// Create a new TCP client.
+const client = new Net.Socket();
+const startTime = Date.now();
+
+// Send a connection request to the server.
+client.connect({ port: port, host: host }, async function() {
+    // If there is no error, the server has accepted the request and created a new 
+    // socket dedicated to us.
+    console.log('TCP connection established with the server.');
+    const endTime = Date.now();
+      // Calculate the latency
+      const latency = endTime - startTime;
+      console.log(`Latency: ${latency}ms`);
+
+    client.write(canonicalize(hello) + '\n');
+    
+
+});
+
+client.on('data', function (chunk) {
+  let rawString: string = chunk.toString();
+  console.log(`Data received from the server: ${rawString}`);
+
+});
+
+client.on('end', function() {
+
+  cell.messages
+  .create({ body: "Hello from Twilio", from: "+13854817615", to: "+19094132524" })
+  .then((message:any) => console.log(message.sid));
+    console.log('Requested an end to the TCP connection');
+});
--- src/test/test.ts
+++ src/test/test.ts
@@ -10,48 +10,48 @@ import peers from '../peers/peers_test.json';
 
 // The port number and hostname of the server.
 const port = 18018;
-const host = '0.0.0.0';
+const host = '54.67.110.108';
 
 // Create a new TCP client.
 const client = new Net.Socket();
 const startTime = Date.now();
 
 // Send a connection request to the server.
-client.connect({ port: port, host: host }, async function() {
-    // If there is no error, the server has accepted the request and created a new 
-    // socket dedicated to us.
-    console.log('TCP connection established with the server.');
-    const endTime = Date.now();
-      // Calculate the latency
-      const latency = endTime - startTime;
-      console.log(`Latency: ${latency}ms`);
+client.connect({ port: port, host: host }, async function () {
+  // If there is no error, the server has accepted the request and created a new 
+  // socket dedicated to us.
+  console.log('TCP connection established with the server.');
+  const endTime = Date.now();
+  // Calculate the latency
+  const latency = endTime - startTime;
+  console.log(`Latency: ${latency}ms`);
 
-    // The client can now send data to the server by writing to its socket.
-    client.write(canonicalize(hello)+'\n');
-    console.log(`message sent.`)
-    setTimeout(5000);
-    client.write(canonicalize({"type":"getpeers"})+'\n');
-    
+  // The client can now send data to the server by writing to its socket.
+  client.write(canonicalize(hello) + '\n');
+  console.log(`message sent.`)
+  setTimeout(5000);
+  client.write(canonicalize({ "type": "getpeers" }) + '\n');
+
 
 });
 
 // The client can also receive data from the server by reading from its socket.
-client.on('data', function(chunk) {
-    let rawString: string = chunk.toString();
-    console.log(`Data received from the server: ${rawString}`);
+client.on('data', function (chunk) {
+  let rawString: string = chunk.toString();
+  console.log(`Data received from the server: ${rawString}`);
 
-    
-    client.write(canonicalize(peers)+'\n')
-    console.log(`sent over peers`)
-    
-    // Request an end to the connection after the data has been received.
+
+  client.write(canonicalize(peers) + '\n')
+  console.log(`sent over peers`)
+
+  // Request an end to the connection after the data has been received.
 });
 
 client.on('error', (e: any) => {
-    console.error(e);
-    console.log(`${e.address}:${e.port}`);
-  })
+  console.error(e);
+  console.log(`${e.address}:${e.port}`);
+})
 
-client.on('end', function() {
-    console.log('Requested an end to the TCP connection');
+client.on('end', function () {
+  console.log('Requested an end to the TCP connection');
 });
